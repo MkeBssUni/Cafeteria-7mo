5974b85d01895aa17a9a1703822a8c3e0f4f2c42
Cristina <20213tn151@utez.edu.mx>
2023-11-02 22:34:23 -0600

    [ADD]Estructura de front

---
+++ SICAF-FrontEnd/websicaf/src/modules/auth/LoginScreens.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginScreens = () => {
+  return (
+    <div>login</div>
+  )
+}
+
+export default LoginScreens
---
+++ SICAF-FrontEnd/websicaf/src/shared/components/AppRouter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginScreens from '../../modules/auth/LoginScreens';
+
+const AppRouter = () => {
+
+  return (
+    <Router>
+        <Routes>
+            {/* Recuerda colocar las rutas aqui */}
+            <Route path="/auth" elemnt={<LoginScreens/>} />
+        </Routes>
+    </Router>
+  )
+}
+
+export default AppRouter
