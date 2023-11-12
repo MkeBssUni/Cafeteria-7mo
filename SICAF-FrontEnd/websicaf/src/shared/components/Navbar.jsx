c6be0c6dd38ce5cffce290137adb0e32caa2d0d7
Cristina <20213tn151@utez.edu.mx>
2023-11-11 18:50:17 -0600

    [UPDATE] navbar

--- SICAF-FrontEnd/websicaf/src/shared/components/Navbar.jsx
+++ SICAF-FrontEnd/websicaf/src/shared/components/Navbar.jsx
@@ -6,44 +6,40 @@ import "../css/color.css";
 
 const Navbarsicaf = () => {
   return (
-    <Navbar
-      style={{ backgroundColor: "var(--color-primary)" }}
-      data-bs-theme="dark"
-    >
-      <Container className="d-flex justify-content-between align-items-center">
+    <Navbar style={{ backgroundColor: "var(--color-primary)", height: '70px' }} data-bs-theme="dark">
+    <Container className="d-flex justify-content-between align-items-center">
       <div style={{ display: 'flex', alignItems: 'center' }}>
-      <Figure.Image className="logo-nav" alt="LOGOSICAFCREMA" src={logo} />
-      <div style={{ marginLeft: '20px' }}>
-        <FeatherIcon
-          icon="menu"
-          color="white"
-          size={40}
-          style={{ strokeWidth: 1.5 }}
-        />
+        <Figure.Image className="logo-nav" alt="LOGOSICAFCREMA" src={logo} />
+        <div style={{ marginLeft: '20px' }}>
+          <FeatherIcon
+            icon="menu"
+            color="white"
+            size={30}
+            style={{ strokeWidth: 1.5 }}
+          />
+        </div>
       </div>
-    </div>
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Navbar.Text>
-            <Button
-              variant="info"
-              size="lg"
-              style={{
-                display: "flex",
-                alignItems: "center",
-                borderRadius: "50px",
-              }}
-            >
-              <FeatherIcon
-                icon="user"
-                color="white"
-                style={{ strokeWidth: 1.5, marginRight: "0.5rem" }}
-              />
-              Perfil
-            </Button>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Navbar.Text>
+          <Button
+            variant="info"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              borderRadius: "50px",
+            }}
+          >
+            <FeatherIcon
+              icon="user"
+              color="white"
+              style={{ strokeWidth: 1.5, marginRight: "0.5rem" }}
+            />
+            Perfil
+          </Button>
+        </Navbar.Text>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   );
 };
 
