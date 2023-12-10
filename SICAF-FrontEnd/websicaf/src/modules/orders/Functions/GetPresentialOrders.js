5bc7a7efdf863424249ac6c8bcee2e7bcdbca933
Cristina <20213tn151@utez.edu.mx>
2023-12-10 01:33:55 -0600

    [UPDATE]Funciones de Ordenes

---
+++ SICAF-FrontEnd/websicaf/src/modules/orders/Functions/GetPresentialOrders.js
@@ -0,0 +1,24 @@
+import AxiosClient from "../../../shared/plugins/axios"
+import Alert,{errorMsg,errorTitle,successMsg,successTitle} from "../../../shared/plugins/Alert"
+import React from 'react'
+
+const GetPresentialOrders = async(request) => {
+    try {
+        const response = await AxiosClient({
+            url:'orders/presential',
+            method: 'GET',
+            data: JSON.stringify(request)
+        })
+        if (!response.error) return response.data;
+      } catch (error) {
+        Alert.fire({
+            title: errorTitle,
+            text: errorMsg,
+            icon:'error',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Aceptar'
+        })   
+      }
+}
+
+export default GetPresentialOrders;
